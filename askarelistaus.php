140182ca7b4adae11f517333d1e84cf35122dc11
Juha H Koponen <jhkopone@svm-19.cs.helsinki.fi>
2013-02-20 14:40:05 +0200

    ohjelmointia

--- askarelistaus.php
+++ askarelistaus.php
@@ -16,11 +16,13 @@ $kysely->execute();
 	valikko();
 	?>
     	<p>Kaikki toimitettavat askareet</p>
+	<form action>
 	<table border>
 	<tr>
 	<th> askareen nimi </th>
 	<th> luokka </th>
 	<th> tärkeysaste </th>
+	<th> poistettavat </th>
 	</tr>
 	<?php
 	while ($rivi = $kysely->fetch()) {
@@ -28,9 +30,15 @@ $kysely->execute();
 		echo "<td>" . $rivi["nimi"] . "</td>";	
 		echo "<td>" . $rivi["luokka"] . "</td>";
 		echo "<td>" . $rivi["taste"] . "</td>";
+		echo "<td><input type=\"checkbox\" name=\"poistettavatAskareet[]\" value=\"\"></td>";
 		echo "</tr>";
 	}
 	?>
 	</table>
+	<br>
+	<?php 
+	echo "<input type=\"submit\" value= \"poista\">";
+	?>
+	</form>
   </body>
 </html>
---
+++ tarkeysasteet.php
@@ -0,0 +1,18 @@
+<?php
+include("yla.php"); 
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Tärkeysasteet</title>
+  </head>
+  <body>
+	<?php
+	kayttajatiedot();
+	valikko();
+	?>
+    	<p>Kaikki tärkeysasteet</p>
+
+  </body>
+</html>
